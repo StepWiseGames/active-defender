116411f3642cd54ea9c4cdeef903499e4d2251bf
alexstouffer <bizkite@hotmail.com>
2018-07-26 18:54:06 -0700

    restructured components directory

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from '../Map/Map';
 
 class App extends Component {
   render() {
@@ -8,11 +9,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Active Defender</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Current task: Navigate a piece on a map.
         </p>
+        <Map />
       </div>
     );
   }
---
+++ src/Components/Map/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Map.css';
+
+class Map extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Map Goes Here:</p>
+            </div>
+        )
+    }
+}
+
+export default Map;
