324d324a3f173a522c3c983441bdfc0bc64fcf2b
alexstouffer <alex@thefullertonian.com>
2018-09-26 17:00:55 -0700

    generateMap function renders squares

---
+++ src/Components/Cell/Cell.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cell.css';
+
+class Cell extends React.Component { 
+    render() {
+        return(
+            <div class="Square"></div>
+        )
+    }
+}
+
+export default Cell;
--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './Map.css';
 
+import Cell from '../Cell/Cell.js';
+
 class Map extends React.Component {
+    constructor(props, numberOfRows, numberOfColumns){
+        super(props),
+
+        this.state = {
+            width: 3,
+            height: 3,
+        }
+    }
+    generateMap(numberOfRows, numberOfColumns) {
+        let board = [];
+        for(var i = 0; i < numberOfRows; i++){
+            let row = [];
+            for(var j = 0; j < numberOfColumns; j++){
+                row.push(<Cell />);
+            }
+            board.push(row);
+        }
+        return board;
+    }
     render() {
         return (
             <div>
-                <p>Map Goes Here:</p>
+                <p>Single Cell:</p>
+                <Cell />   
+                <p>Row of Three Cells:</p>
+                {this.generateMap(this.state.width,this.state.height)}
             </div>
         )
     }
